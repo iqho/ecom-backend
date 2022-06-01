777f2753d2b11ad759443d913483035ffff1b971
mbd24 <45692431+mbd24@users.noreply.github.com>
2022-06-01 13:31:28 +0600

    change token name

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -47,7 +47,7 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['message' => 'Hi '.$user->name.', welcome to home','access_token' => $token, 'token_type' => 'Bearer', ]);
+        return response()->json(['user' => $user->name, 'token' => $token, 'token_type' => 'Bearer', ]);
     }
 
     // method for user logout and delete token
