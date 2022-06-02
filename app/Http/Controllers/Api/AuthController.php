38505fc3c00daa53595d1189bf6f9aa5179f898f
Iqbal Hossen <hosiqb@gmail.com>
2022-06-02 13:02:14 +0600

    update auth page

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -31,23 +31,21 @@ class AuthController extends Controller
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()
-            ->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer', ]);
+        return response()->json(['user' => $user,'token' => $token, 'token_type' => 'Bearer', ]);
     }
 
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('email', 'password')))
         {
-            return response()
-                ->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         $user = User::where('email', $request['email'])->firstOrFail();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()->json(['user' => $user->name, 'token' => $token, 'token_type' => 'Bearer', ]);
+        return response()->json(['user' => $user, 'token' => $token, 'token_type' => 'Bearer', ]);
     }
 
     // method for user logout and delete token
